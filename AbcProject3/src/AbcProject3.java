08abaa5c69f80f845f497e2156f1332d17502fd8
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-03-09 12:08:55 -0800

    09-Mar-23 added file

---
+++ AbcProject3/src/AbcProject3.java
@@ -0,0 +1,6 @@
+public class AbcProject3 {
+
+    public static void main(String [] args ){
+        System.out.println("Hello todsy we are learn how to use INTELLIJ and how to write and run simple programmer");
+    }
+}
