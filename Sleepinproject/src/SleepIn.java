586ae5dbcd53389b51a1c3a542173d33f7c69593
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-03-09 16:52:27 -0800

    09-mar-23

--- Sleepinproject/src/SleepIn.java
+++ Sleepinproject/src/SleepIn.java
@@ -1,7 +1,7 @@
 public class SleepIn {
 
     public static void main(String[] args ){
-
+        System.out.println(sleepIn(false, false));
         System.out.println(sleepIn(true, false));
         System.out.println(sleepIn(false, true));
 
