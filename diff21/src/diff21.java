54c85fea4f7abf9d34606a6479537fc1127fda2d
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-03-13 11:35:37 -0700

    13-mar-23

---
+++ diff21/src/diff21.java
@@ -0,0 +1,15 @@
+public class diff21 {
+    public static void main(String [] args ){
+        System.out.println(9>20);
+        System.out.println(15>25);
+        System.out.println(12<19);
+    }
+ public int diff(int n) {
+        if (n>21){
+            return 2*(Math.abs(n-21));
+        }
+        else{
+            return Math.abs(n-21);
+        }
+ }
+}
