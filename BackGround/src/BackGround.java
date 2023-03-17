879d3166ffbe035ba910a0a2e5c88ba694cea002
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-03-17 13:09:48 -0700

    17-marc

---
+++ BackGround/src/BackGround.java
@@ -0,0 +1,11 @@
+public class BackGround {
+
+    public static void main(String [] args ){
+
+}
+
+    public String bcakAround(String str) {
+        String last = str.substring(str.length() - 1);
+        return last + str + last;
+    }
+    }
---
+++ Or35/src/Or35.java
@@ -0,0 +1,12 @@
+public class Or35 {
+
+    public static void main(String [] args){
+        System.out.println();
+        System.out.println();
+
+    }
+
+    public boolean or35(int n){
+        return (n % 3 == 0) || ( n %5 == 0);
+    }
+}
