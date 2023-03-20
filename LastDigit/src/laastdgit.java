8a3b96de7d11e43daf24ec4bafeb1a440ab769e7
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-03-20 10:41:19 -0700

    20-mar

---
+++ LastDigit/src/laastdgit.java
@@ -0,0 +1,9 @@
+public class laastdgit {
+    public static void main(String[]args){
+        System.out.println("7,17");
+
+    }
+    public boolean LastDigit(int a, int b){
+        return(a% 10 == b % 10);
+    }
+}
