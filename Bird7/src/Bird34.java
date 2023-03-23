fc2247b534e995b3eca230b8eb7856cff45a9fc6
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-03-23 15:38:59 -0700

    2323

---
+++ Bird7/src/Bird34.java
@@ -0,0 +1,15 @@
+public class Bird34 {
+    public String name;
+    public Boolean CanSwim;
+    public String colour;
+
+    @Override
+    public String toString() {
+        return "Bird34{" +
+                "name='" + name + '\'' +
+                ", CanSwim=" + CanSwim +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
+}
+
---
+++ Bird7/src/MainClass09.java
@@ -0,0 +1,10 @@
+public class MainClass09 {
+    public static void main(String[] args) {
+        Bird34 bird=new Bird34();
+        bird.name="Sparrow";
+        bird.CanSwim=false;
+        bird.colour="yellow";
+
+        System.out.println(bird);
+    }
+}
