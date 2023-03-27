1f3e4021a46c390d6f8dca3fba17faa2071149d8
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-03-27 14:21:59 -0700

    2723

---
+++ Test/src/product.java
@@ -0,0 +1,8 @@
+public class product {
+    public static void main(String[] args) {
+    Multi(67,98);
+    }
+    public static void Multi(int a,int b){
+        System.out.println(a*b);
+    }
+}
