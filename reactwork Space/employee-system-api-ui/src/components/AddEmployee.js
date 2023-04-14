95ae0b7012ee047783016b1f6ea0fc7978b970a5
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-04-13 18:04:41 -0700

    1204

--- reactwork Space/employee-system-api-ui/src/components/AddEmployee.js
+++ reactwork Space/employee-system-api-ui/src/components/AddEmployee.js
@@ -4,7 +4,7 @@ const AddEmployee = () => {
   return (
     <div className ="flex max-w-2xl mx-auto shadow border-b">
         <div className="px-8 py-8">
-            <div className="font-thin text-2xl tracking-wider">
+            <div className="font-thin text-2xl ">
                 <h1>Add New Employee</h1>
             </div>
         </div>
