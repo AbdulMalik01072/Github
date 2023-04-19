bf5db19c8b3f06a4470e6987eac8269b015ca398
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-04-18 18:04:57 -0700

    1604

--- reactwork Space/employee-system-api/src/main/java/com/NoofProjects2/Employee/controller/EmployeeController.java
+++ reactwork Space/employee-system-api/src/main/java/com/NoofProjects2/Employee/controller/EmployeeController.java
@@ -3,11 +3,11 @@ package com.NoofProjects2.Employee.controller;
 import com.NoofProjects2.Employee.model.Employee;
 import com.NoofProjects2.Employee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -21,5 +21,9 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
        return employeeService.createEmployee(employee);
+    }
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
     }
 }
--- reactwork Space/employee-system-api/src/main/java/com/NoofProjects2/Employee/services/EmployeeService.java
+++ reactwork Space/employee-system-api/src/main/java/com/NoofProjects2/Employee/services/EmployeeService.java
@@ -2,6 +2,10 @@ package com.NoofProjects2.Employee.services;
 
 import com.NoofProjects2.Employee.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
 }
