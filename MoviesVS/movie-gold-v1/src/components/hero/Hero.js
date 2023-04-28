0f17189c9de4cde51ae3dc71616a011cf5255a26
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-04-27 18:36:16 -0700

    2704

--- MoviesVS/movie-gold-v1/src/components/hero/Hero.js
+++ MoviesVS/movie-gold-v1/src/components/hero/Hero.js
@@ -3,15 +3,25 @@ import Corousel from'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link, useNavigate} from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Hero = ({movies}) => {
+
+        const navigate = useNavigate();
+
+        function reviews(movieId)
+        {
+            navigate('/Reviews/${movieId}');
+        }
+
   return (
     <div className='movie-carousel-container'>
         <Corousel>
             {
                 movies.map((movie) =>{
                     return(
-                    <Paper>
+                    <Paper key={movie.imdbId}>
                         <div className='movie-card-container'>
                         <div className="movie-card" style={{"--img": 'url(${movie.backdrops[0]})'}}>
                             <div className="movie-detail">
@@ -22,11 +32,17 @@ const Hero = ({movies}) => {
                                 <h4>{movie.title}</h4>
                                 </div>
                                 <div  className="movie-button-container">
+                                    <Link t0={'/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}'}>
                                     <div className="play-button-icon-container">
                                         <FontAwesomeIcon className="play-button-icon"
                                         icon={faCirclePlay}
                                         />
 
+                                    </div>
+                                    </Link>
+
+                                    <div className="movie-review-button-container">
+                                        <Button variant="info" onClick={()=> reviews(movie.imdbId)}>Reviews</Button>
                                     </div>
 
                                 </div>
---
+++ MoviesVS/movie-gold-v1/src/components/notFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+        This page could not found
+    </div>
+  )
+}
+
+export default NotFound
