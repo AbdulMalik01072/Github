fe5f086afbfe2c2589ff51bb0723619cfac3dfc1
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-05-01 17:46:12 -0700

    0105

---
+++ Teaching/src/SomeClass.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class SomeClass {
+
+    public static void main(String [] args){
+        List <String> myList = new ArrayList();
+
+        myList.add("Sunial");
+        myList.add("Abdul");
+        myList.add("Khan");
+        myList.add("Abdul1");
+        System.out.println(myList);
+
+
+
+    }
+}
