97b0f813255fdf5b1d7919b7e175b7fbab7ae4a4
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-05-10 17:13:12 -0700

    055

---
+++ untitled1/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main(String [] args){
+        System.out.println();
+
+    }
+
+
+    public class StringBufferReverseExample1 {
+        public static void main(String[] args) {
+            StringBuffer sb = new StringBuffer("Hello World");
+            sb.reverse();
+            System.out.println(sb);
+        }
+    }
+}
---
+++ untitled1/src/TokenizerClass.java
@@ -0,0 +1,19 @@
+import java.util.StringTokenizer;
+
+public class TokenizerClass {
+
+    public static void main(String[] args) {
+        String s = "my name is ABDUL MALIK";
+        reverseString(s);
+
+
+    }
+    public static void reverseString (String s ) {
+        String S1;
+        StringTokenizer ST = new StringTokenizer(s, "");
+        while (ST.hasMoreTokens()) {
+             S1 = ST.nextToken();
+            System.out.println(new StringBuilder().reverse(S1).toString());
+        }
+    }
+}
