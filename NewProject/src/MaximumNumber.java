328f80a738b1fca2b9ddc73ff0cf54c7e8916d2a
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-05-17 19:17:21 -0700

    005

---
+++ NewProject/src/MaximumNumber.java
@@ -0,0 +1,15 @@
+import static jdk.internal.icu.lang.UCharacter.MAX_VALUE;
+
+public class MaximumNumber {
+    public static void main(String[]args){
+        int []a ={10,4,3,50,23,90};
+        int []a  = MAX_VALUE;
+
+        for(int i=0;i<a.length;i++){
+            for(int j=1;j<a.length;j++){
+            System.out.println(a[i]++a.length);
+
+        }
+    }
+ }
+}
