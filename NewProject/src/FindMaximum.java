a9212c1b9c7523df2893d8c8d3f20e69879c2bc4
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-05-17 18:46:35 -0700

    051

---
+++ NewProject/src/FindMaximum.java
@@ -0,0 +1,12 @@
+public class FindMaximum {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        for (int i = 0; i < 5; i++) {
+            if(i%2 !=0){
+                System.out.println(a[i]);
+
+            }
+        }
+    }
+}
