f10e97f170df6eee33ec81e62b94fb30b393377c
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-05-18 17:59:43 -0700

    0518

---
+++ NewProject/src/CountingZero.java
@@ -0,0 +1,23 @@
+public class CountingZero {
+    public static void main(String[] args) {
+        //move the zeros in the last of the array
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[count] = arr[i];
+                count++;
+            }
+        }
+        while (count < arr.length) {
+            arr[count++] = 0;
+            }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+    }
+
+
+
