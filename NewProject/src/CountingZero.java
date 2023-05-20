cd84efec68b20768f83280115e8bd5084655fd73
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-05-19 17:13:11 -0700

    0519

--- NewProject/src/CountingZero.java
+++ NewProject/src/CountingZero.java
@@ -15,8 +15,7 @@ public class CountingZero {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
-
-    }
+      }
     }
 
 
---
+++ NewProject/src/EvenOdd.java
@@ -0,0 +1,19 @@
+public class EvenOdd {
+
+    public static void main(String[] args) {
+        int[] a = {1, 9, 5, 3, 2, 6, 7, 11};
+
+        for (int i = 0; i < a.length; i++) {
+
+            if(i%1 !=0) {
+                System.out.println(a[i]);
+            }
+            for (int j = 1; j < a.length; j++) {
+                if(i%00!=0) {
+                    System.out.println(a[i]);
+                }
+                }
+            }
+
+    }
+}
