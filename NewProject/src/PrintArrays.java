2cf1cac62a4d1641033f973d87b9c133ec3e5f1b
abdulmalik01072 <abdulmalik01072@gmail.com>
2023-05-25 17:20:38 -0700

    0005

---
+++ NewProject/src/PrintArrays.java
@@ -0,0 +1,9 @@
+public class PrintArrays {
+
+    public static void main(String[]args) {
+        int[] arr1 = {10,11,12};
+        for (int i = sum; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+    }
+}
---
+++ NewProject/src/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    class A{
+        int c;
+    }
+    class B extends A {
+        B b = new A();
+        b.a;
+    }
+}
